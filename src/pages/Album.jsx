2e301ba6a2231c5bdeffcd1e020fcdd7cc1a1c53
gus-aguiar <gustavopma90@gmail.com>
2022-12-14 17:05:10 -0300

    fazendo a 7, vou terminar por aqui por hoje

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  async componentDidMount() {
+    const { match } = this.props;
+    const musics = await getMusics(match.params.id);
+    console.log(musics);
+  }
+
   render() {
     return (
       <div data-testid="page-album">
