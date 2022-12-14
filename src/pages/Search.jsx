33ab30b61fdbbdd5d0ca4dc50babb1443f16289c
gus-aguiar <gustavopma90@gmail.com>
2022-12-14 16:01:57 -0300

    fiz a seis

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   state = {
     isDisabled: true,
     searchValue: '',
     isLoading: false,
+    errorMusic: false,
+    artist: undefined,
+    albums: [],
 
   };
 
@@ -17,13 +22,58 @@ class Search extends React.Component {
     return this.setState({ isDisabled: true, searchValue: target.value });
   };
 
-  render() {
-    const { isLoading } = this.state;
-    const { isDisabled } = this.state;
+  handleClick = async () => {
     const { searchValue } = this.state;
+    this.setState({
+      isLoading: true,
+      errorMusic: false,
+      artist: undefined,
+      searchValue: '',
+    }, async () => {
+      const albums = await searchAlbumsAPI(searchValue);
+      this.setState({
+        isLoading: false,
+        albums,
+        artist: searchValue,
+        errorMusic: true,
+      });
+    });
+  };
+
+  showContent = (content) => content
+    .map(({ collectionName, artistName, collectionId }) => (
+      <div key={ collectionId }>
+        <p>{`Album: ${collectionName}`}</p>
+        <p>{`ArtistName: ${artistName}`}</p>
+        <Link
+          data-testid={ `link-to-album-${collectionId}` }
+          to={ `/album/${collectionId}` }
+        >
+          Album
+        </Link>
+      </div>
+    ));
+
+  render() {
+    const { isLoading, isDisabled, searchValue, artist, albums, errorMusic } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        {' '}
+        {!errorMusic
+          ? <div />
+          : (
+            <div>
+              {
+                albums.length === 0 && errorMusic
+                  ? (
+                    <p>Nenhum álbum foi encontrado</p>
+                  ) : (
+                    <p>{`Resultado de álbuns de: ${artist}`}</p>
+                  )
+              }
+            </div>
+          )}
         {isLoading
           ? <p>Carregando...</p>
           : (
@@ -37,10 +87,12 @@ class Search extends React.Component {
               <button
                 data-testid="search-artist-button"
                 type="submit"
+                onClick={ this.handleClick }
                 disabled={ isDisabled }
               >
                 Pesquisar
               </button>
+              {this.showContent(albums)}
             </div>)}
 
       </div>
