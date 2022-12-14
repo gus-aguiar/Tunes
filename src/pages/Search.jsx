c139642d52d1e989a432714f98a2c60813d62693
gus-aguiar <gustavopma90@gmail.com>
2022-12-14 11:42:41 -0300

    fiz a 5, fazendo a 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,34 +4,45 @@ import Header from '../components/Header';
 class Search extends React.Component {
   state = {
     isDisabled: true,
+    searchValue: '',
+    isLoading: false,
 
   };
 
   handleChange = ({ target }) => {
     const validationInput = 2;
     if (target.value.length >= validationInput) {
-      return this.setState({ isDisabled: false });
+      return this.setState({ isDisabled: false, searchValue: target.value });
     }
-    return this.setState({ isDisabled: true });
+    return this.setState({ isDisabled: true, searchValue: target.value });
   };
 
   render() {
+    const { isLoading } = this.state;
     const { isDisabled } = this.state;
+    const { searchValue } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <input
-          data-testid="search-artist-input"
-          type="text"
-          onChange={ this.handleChange }
-        />
-        <button
-          data-testid="search-artist-button"
-          type="submit"
-          disabled={ isDisabled }
-        >
-          Entrar
-        </button>
+        {isLoading
+          ? <p>Carregando...</p>
+          : (
+            <div>
+              <input
+                data-testid="search-artist-input"
+                type="text"
+                onChange={ this.handleChange }
+                value={ searchValue }
+              />
+              <button
+                data-testid="search-artist-button"
+                type="submit"
+                disabled={ isDisabled }
+              >
+                Pesquisar
+              </button>
+            </div>)}
+
       </div>
     );
   }
