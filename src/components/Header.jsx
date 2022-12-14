4081777c891b51e70818cccd9d741d5f63fc97a9
gus-aguiar <gustavopma90@gmail.com>
2022-12-14 10:58:10 -0300

    fiz a 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -22,8 +23,18 @@ class Header extends React.Component {
 
     return (
       <header data-testid="header-component">
-        {isLoading ? <p>Carregando...</p>
-          : <p data-testid="header-user-name">{name}</p>}
+        {isLoading ? (
+          <p>Carregando...</p>
+        ) : (
+          <p data-testid="header-user-name">{name}</p>
+        )}
+        <nav>
+          <Link to="/search" data-testid="link-to-search">
+            Busca
+          </Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+          <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+        </nav>
       </header>
     );
   }
